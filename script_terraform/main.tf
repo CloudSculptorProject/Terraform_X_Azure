c9a39f4d2ed9009e2ddc06c3ce9a27b2d4c2dc65
CloudSculptor <156903952+CloudSculptorProject@users.noreply.github.com>
2024-05-19 13:12:04 +0200

    Update main.tf

--- script_terraform/main.tf
+++ script_terraform/main.tf
@@ -1,5 +1,5 @@
 # Resource Group creation
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "windows_rg" {
   name     = "Maquinas-Virtuales"
   location = "eastus"
 }
@@ -12,29 +12,29 @@ data "azurerm_shared_image" "main" {
 }
 
 # Virtual Network creation
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "windows_vnet" {
   name                = "winvnetexamen"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.windows_rg.name
+  location            = azurerm_resource_group.windows_rg.location
   address_space       = ["10.0.0.0/16"]
 }
 
 # Subnet creation within the Virtual Network
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "windows_subnet" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.windows_rg.name
+  virtual_network_name = azurerm_virtual_network.windows_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
-
+ 
 # Windows VM creation
-resource "azurerm_virtual_machine" "vm" {
+resource "azurerm_virtual_machine" "windows_vm" {
   count               = var.vm_count
 
   name                = "win-vm${count.index}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
+  resource_group_name = azurerm_resource_group.windows_rg.name
+  location            = azurerm_resource_group.windows_rg.location
+  network_interface_ids = [azurerm_network_interface.windows_nic[count.index].id]
   vm_size             = "Standard_B2s"
 
   storage_image_reference {
@@ -62,36 +62,36 @@ resource "azurerm_virtual_machine" "vm" {
 }
 
 # Network Interface creation for each VM
-resource "azurerm_network_interface" "nic" {
+resource "azurerm_network_interface" "windows_nic" {
   count = var.vm_count
 
   name                = "win-example-nic${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.windows_rg.location
+  resource_group_name = azurerm_resource_group.windows_rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.windows_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.ip[count.index].id
+    public_ip_address_id          = azurerm_public_ip.windows_ip[count.index].id
   }
 }
 
 # Public IP creation for each VM
-resource "azurerm_public_ip" "ip" {
+resource "azurerm_public_ip" "windows_ip" {
   count               = var.vm_count
   name                = "win-example-ip${count.index}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.windows_rg.name
+  location            = azurerm_resource_group.windows_rg.location
   allocation_method   = "Dynamic"
 }
-
+ 
 # Network Security Group creation for each VM
-resource "azurerm_network_security_group" "nsg" {
+resource "azurerm_network_security_group" "windows_nsg" {
   count               = var.vm_count
   name                = "win-nsg-${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.windows_rg.location
+  resource_group_name = azurerm_resource_group.windows_rg.name
   
   security_rule {
     name                       = "Allow-RDP-${count.index}"
@@ -109,11 +109,11 @@ resource "azurerm_network_security_group" "nsg" {
 # Associate NSG with each VM's NIC
 resource "azurerm_network_interface_security_group_association" "nsg_association" {
   count               = var.vm_count
-  network_interface_id = azurerm_network_interface.nic[count.index].id
-  network_security_group_id = azurerm_network_security_group.nsg[count.index].id
+  network_interface_id = azurerm_network_interface.windows_nic[count.index].id
+  network_security_group_id = azurerm_network_security_group.windows_nsg[count.index].id
 }
 
 # Output the public IP addresses of the Windows VMs
 output "windows_public_ips" {
-  value = [azurerm_public_ip.ip[*].ip_address]
+  value = [azurerm_public_ip.windows_ip[*].ip_address]
 }
