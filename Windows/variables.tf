41d4320b77ef60b57bde3d1b072a465ca7787332
CloudSculptor <156903952+CloudSculptorProject@users.noreply.github.com>
2024-05-26 12:39:33 +0200

    Update variables.tf

--- Windows/variables.tf
+++ Windows/variables.tf
@@ -1,4 +1,4 @@
-variable "vm_count" {
+variable "vm_count" { # La variable vm_count se utiliza para especificar cuántas instancias de ciertos recursos (como máquinas virtuales, interfaces de red, direcciones IP públicas, etc.) se deben crear.  
   description = "Number of Windows Virtual Machine you want to create"
   type        = number
 }
