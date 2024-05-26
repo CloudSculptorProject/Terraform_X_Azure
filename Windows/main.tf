f05eb01b37305abab22a679175bb21c514cd9c54
CloudSculptor <156903952+CloudSculptorProject@users.noreply.github.com>
2024-05-26 12:35:41 +0200

    Update main.tf

--- Windows/main.tf
+++ Windows/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "windows_rg" {
 # Locate the existing custom image from the Shared Image Gallery
 data "azurerm_shared_image" "main" { # Este bloque se utiliza para acceder a datos de recursos que ya existen en la infraestructura de Azure.  En este caso, está accediendo a una imagen compartida de Azure (Shared Image).
   name                = "VM_ISO" # Especifica el nombre de la imagen compartida que quieres obtener.
-  gallery_name        = "VM_Principal" # Indica el nombre de la galería de imágenes (Compute Gallery) donde se encuentra la imagen
+  gallery_name        = "VM_Principal" # Indica el nombre de la galería de imágenes (Compute Gallery) donde se encuentra la imagen.
   resource_group_name = "CloudSculptor"
 }
 
@@ -29,26 +29,25 @@ resource "azurerm_subnet" "windows_subnet" {
  
 # Windows VM creation
 resource "azurerm_virtual_machine" "windows_vm" {
-  count               = var.vm_count
-
-  name                = "win-vm${count.index}"
+  count               = var.vm_count # Esta línea indica que se creará un número específico de instancias de este recurso. El valor que se utilice para count vendrá de una variable llamada vm_count.
+  name                = "win-vm${count.index}" # Está compuesto por "win-vm" seguido del índice de la instancia. Por ejemplo, si vm_count es 3, las instancias se llamarán "win-vm0", "win-vm1" y "win-vm2".
   resource_group_name = azurerm_resource_group.windows_rg.name
   location            = azurerm_resource_group.windows_rg.location
   network_interface_ids = [azurerm_network_interface.windows_nic[count.index].id]
   vm_size             = "Standard_B2s"
 
-  storage_image_reference {
+  storage_image_reference { # Este bloque de código asegura que la máquina virtual se creará utilizando la imagen de almacenamiento especificada por el identificador obtenido del origen de datos azurerm_shared_image.
     id = data.azurerm_shared_image.main.id
   }
 
-  storage_os_disk {
+  storage_os_disk { # Este bloque de código se refiere a la configuración del disco del sistema operativo (OS) para la máquina virtual en Azure.
     name              = "win-myosdisk${count.index}"
     caching           = "ReadWrite"
-    create_option     = "FromImage"
+    create_option     = "FromImage" # Esto especifica cómo se creará el disco. En este caso, se establece en "FromImage", lo que significa que el disco se creará a partir de la imagen especificada en la configuración de la máquina virtual.
     managed_disk_type = "Standard_LRS"
   }
 
-  os_profile {
+  os_profile { # Este bloque de código se refiere a la configuración del perfil del sistema operativo (OS) para la máquina virtual en Azure.
     computer_name  = "win-vm${count.index}"
     admin_username = var.admin_username
     admin_password = var.admin_password
